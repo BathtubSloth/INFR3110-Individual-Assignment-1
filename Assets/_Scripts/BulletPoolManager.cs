190c9c281b24d4397b27b4592e4e9f93e16df41a
Nathan Tuck <nathan.tuck1@ontariotechu.net>
2020-10-28 13:48:12 -0400

    Finished assignment
    no bonus tho

--- Assets/_Scripts/BulletPoolManager.cs
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 // TODO: Bonus - make this class a Singleton!
@@ -8,13 +10,24 @@ using UnityEngine;
 public class BulletPoolManager : MonoBehaviour
 {
     public GameObject bullet;
+    public int MaxBullets;
 
     //TODO: create a structure to contain a collection of bullets
+    private Queue<GameObject> BulletPool;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Create reference to bullet controller
+        bullet.GetComponent<BulletController>().BPM = this;
         // TODO: add a series of bullets to the Bullet Pool
+        //Creates queue
+        BulletPool = new Queue<GameObject>();
+        //Call functions below
+        _BuildBulletPool();
+
+
+        
     }
 
     // Update is called once per frame
@@ -23,16 +36,53 @@ public class BulletPoolManager : MonoBehaviour
         
     }
 
+    public bool IsPoolEmpty()
+    {
+        if (BulletPool.Count == 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public int SizeOfBulletPool()
+    {
+        return BulletPool.Count;
+    }
+
+    private void _BuildBulletPool()
+    {
+        for (int i = 0; i < MaxBullets; i++)
+        {
+            var tempBullet = Instantiate(bullet);
+            tempBullet.transform.parent = transform;
+            tempBullet.SetActive(false);
+            BulletPool.Enqueue(tempBullet);
+        }
+    }
+
     //TODO: modify this function to return a bullet from the Pool
     public GameObject GetBullet()
     {
-
-        return bullet;
+        GameObject tempBullet;
+        if (IsPoolEmpty() == true)
+        {
+            tempBullet = Instantiate(bullet);
+            tempBullet.transform.parent = transform;
+        }
+        else
+        {
+            tempBullet = BulletPool.Dequeue();
+            tempBullet.SetActive(true);
+        }
+        return tempBullet;
     }
 
     //TODO: modify this function to reset/return a bullet back to the Pool 
     public void ResetBullet(GameObject bullet)
     {
-
+        bullet.SetActive(false);
+        BulletPool.Enqueue(bullet);        
     }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -22,6 +22,8 @@ public class PlayerController : MonoBehaviour
     private bool isFiring = false;
 
     //TODO: create a reference to the BulletPoolManager here
+    public BulletPoolManager BPM;
+
 
     // Start is called before the first frame update
     void Start()
@@ -121,7 +123,13 @@ public class PlayerController : MonoBehaviour
                 //TODO: GetBullet function which will return a reference to a 
                 //TODO: bullet object. 
                 //TODO: Ensure you position the new bullet at the bulletSpawn position
-                Instantiate(bullet, bulletSpawn.position, Quaternion.identity);
+
+                GameObject tempBullet;
+
+                tempBullet = BPM.GetBullet();
+
+                tempBullet.transform.position = bulletSpawn.position;
+                
             }
 
         }
